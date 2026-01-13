1ccf5cc3900a15672da5d6f6f7747b0d5e622f29
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2026-01-13 18:04:07 +0100

    Update gotify_ssh_login.sh

--- conf/gotify_ssh_login.sh
+++ conf/gotify_ssh_login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST "__GOTIFYPATH__/message?token=__TOKEN__" -F "title=__SERVER__ - SSH login" -F "message=$USER connected from $SSH_CLIENT" > /dev/null 2>&1
+curl -X POST "__GOTIFYPATH__message?token=__TOKEN__" -F "title=__SERVER__ - SSH login" -F "message=$USER connected from $SSH_CLIENT" > /dev/null 2>&1
